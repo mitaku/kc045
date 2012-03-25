e4deed083fbeb147cef93bc9e569c8ae987a3091
mitaku <mitaku1104@gmail.com>
2012-03-25 17:45:20 +0900

    Modify Top Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   def countdown_label(date = Date.new(2012, 4, 14))
     case date <=> Date.today
     when  1 # 開催前: あとn日
-      "あと#{(date - Date.today).to_i}日"
+      "開催まであと#{(date - Date.today).to_i}日"
     when  0 # 開催日: 本日開催
       "本日開催"
     when -1 # 開催後: 開催終了
