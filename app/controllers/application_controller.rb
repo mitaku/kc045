839420bfef12e4aa21d03a4dad8b81c770c7de6f
mitaku <mitaku1104@gmail.com>
2012-03-25 15:36:29 +0900

    Add login_required method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :signed_in?
   protected
+  def login_required
+    unless signed_in?
+      redirect_to root_path# please sign in
+    end
+  end
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
