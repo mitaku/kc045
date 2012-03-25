afa056985bdfd78b208d2ca00b6de47276fce692
mitaku <mitaku1104@gmail.com>
2012-03-25 17:03:10 +0900

    Add Profile validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,10 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
 
-  attr_accessible :name, :phonetic_name, :twitter, :blog, :hometown, :comment
+  attr_accessible :name, :phonetic_name, :blog, :hometown, :comment
+
+  validates :name, :presence => true, :length => {:maximum => 60}
+  validates :phonetic_name, :length => {:maximum => 60}
+  validates :blog, :length => {:maximum => 200}
+  validates :comment, :length => {:maximum => 140}
 end
